6d308227632b5bf95f46e31d14ae8d4d6d4277c2
Digital  Alchemyst <digitalalchemiststudios@gmail.com>
2023-08-23 16:26:51 -0500

    config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// @ts-check
 
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  images: {
+    domains: ['cloud.appwrite.io', 'cdn.sanity.io'],
+  },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
